7e6f9803093018872b72132b6285e0e9c7f7a573
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-29 16:19:57 +0900

    Feat: SwiftUI 사용하기

---
+++ ATSOPT36_Assignment/ATSOPT36_Assignment/Presentation/SwiftUI/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ATSOPT36_Assignment
+//
+//  Created by 권석기 on 5/29/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
